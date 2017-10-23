f515426b7a1893b22e3ff2d4fa62e08bb562029c
Dan Crankshaw <dscrankshaw@gmail.com>
2017-10-22 21:36:33 -0700

    Updated model profile code

--- plotting/create_model_profile.py
+++ plotting/create_model_profile.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 import os
-import sys
+# import sys
 import pandas as pd
 
 from utils import COST_PER_GPU, COST_PER_CPU
@@ -15,12 +15,15 @@ def load_results(results_dir):
             with open(os.path.join(results_dir, exp), "r") as f:
                 data = json.load(f)
                 # Skip the first trial
+                format_client_metrics(data)
                 skip_first_trial(data)
-                experiments.append(data)
+                if max([len(cm["thrus"]) for cm in data["client_metrics"]]) > 5:
+                    experiments.append(data)
         else:
             # print("skipping %s" % os.path.join(results_dir, exp))
             pass
     return experiments
+
 
 def skip_first_trial(results_json):
     client_metrics = results_json["client_metrics"]
@@ -28,36 +31,14 @@ def skip_first_trial(results_json):
         for metric in client:
             client[metric] = client[metric][1:]
 
-# # heuristic to determine when latencies have flattened out
-# def select_valid_trials(name, results_json):
-#     p99_lats = results_json["client_metrics"][0]["p99_lats"]
-#
-#     # We assume that at least the last 8 trials were good
-#     last_8_mean = np.mean(p99_lats[-8:])
-#     last_8_stdev = np.mean(p99_lats[-8:])
-#
-#     good_trials = []
-#     for i in reversed(range(len(p99_lats))):
-#         if p99_lats[i] <= last_8_mean + last_8_stdev:
-#             good_trials.append(i)
-#         elif len(p99_lats) - i < 8:
-#             print("Found a bad trial in the last 8 trials for: {}".format(name))
-#         else:
-#             break
-#     first_good_trial = min(good_trials)
-#     last_good_trial = max(good_trials)
-#     assert last_good_trial == len(p99_lats) - 1
-#     return first_good_trial, last_good_trial
 
+def format_client_metrics(data):
+    if type(data["client_metrics"]) == dict:
+        data["client_metrics"] = [data["client_metrics"]]
 
-# def extract_good_results(results_json, first_good_trial, last_good_trial):
-#     client_metrics = results_json["client_metrics"]
-#     for client in client_metrics:
-#         for metric in client:
-#             client[metric] = client[metric][first_good_trial:last_good_trial + 1]
 
 def num_gpus(exp):
-    return exp["node_configs"][0]["gpus_per_replica"]
+    return len(exp["node_configs"][0]["gpus"])
 
 
 def num_cpus(exp):
@@ -83,8 +64,7 @@ def throughput(exp):
     mean = 0
     var = 0
     for cm in exp["client_metrics"]:
-        # discard first trial for each set of client metrics
-        cm_thrus = cm["thrus"][1:]
+        cm_thrus = cm["thrus"]
         cm_mean = np.mean(cm_thrus)
         cm_var = np.var(cm_thrus)
         mean += cm_mean
@@ -94,7 +74,20 @@ def throughput(exp):
     return (mean, std)
 
 
-def extract_client_metrics(exp):
+def extract_client_metrics_old(exp):
+    name = model_name(exp)
+    hists = exp["clipper_metrics"]["histograms"]
+    lat_key = "model:{name}:1:prediction_latency".format(name=name)
+    batch_key = "model:{name}:1:batch_size".format(name=name)
+    for h in hists:
+        if list(h.keys())[0] == lat_key:
+            p99_lat = float(h[lat_key]["p99"]) / 1000.0
+        elif list(h.keys())[0] == batch_key:
+            mean_batch = h[batch_key]["mean"]
+    return (p99_lat, mean_batch)
+
+
+def extract_client_metrics_new(exp):
     clipper_metrics = exp["client_metrics"][0]["all_metrics"]
     p99_lats = []
     mean_batch_sizes = []
@@ -107,25 +100,30 @@ def extract_client_metrics(exp):
         for h in hists:
             if list(h.keys())[0] == lat_key:
                 lat = float(h[lat_key]["p99"]) / 1000.0
-                # if float(h[lat_key]["p99"]) / 1000.0 < 0.01:
-                #     # print(json.dumps(trial, indent=2))
-                #     continue
-                # else:
                 p99_lats.append(lat)
             elif list(h.keys())[0] == batch_key:
                 mean_batch_sizes.append(float(h[batch_key]["mean"]))
-    return (np.mean(p99_lats), np.std(p99_lats), np.mean(mean_batch_sizes))
+    return (np.mean(p99_lats), np.mean(mean_batch_sizes))
+
+
+def extract_client_metrics(exp):
+    if "clipper_metrics" in exp:
+        return extract_client_metrics_old(exp)
+    else:
+        return extract_client_metrics_new(exp)
+
 
 def compute_cost(results_json):
     nodes = results_json["node_configs"]
     total_cost = 0.0
     for n in nodes:
         num_reps = n["num_replicas"]
-        num_gpus = n["gpus_per_replica"] * num_reps
-        num_cpus = n["cpus_per_replica"] * num_reps
-        cost = float(num_gpus) * COST_PER_GPU + float(num_cpus) * COST_PER_CPU
+        n_gpus = num_gpus(results_json) * num_reps
+        n_cpus = num_cpus(results_json) * num_reps
+        cost = float(n_gpus) * COST_PER_GPU + float(n_cpus) * COST_PER_CPU
         total_cost += cost
     return total_cost
+
 
 def create_model_profile_df(results_dir):
     experiments = load_results(results_dir)
@@ -135,7 +133,6 @@ def create_model_profile_df(results_dir):
     mean_thrus = []
     std_thrus = []
     p99_lats = []
-    p99_lat_errs = []
     mean_batches = []
     inst_types = []
     costs = []
@@ -148,9 +145,8 @@ def create_model_profile_df(results_dir):
         mean_thrus.append(mean_thru)
         std_thrus.append(std_thru)
         costs.append(compute_cost(e))
-        p99_lat, p99_lat_err, mean_batch = extract_client_metrics(e)
+        p99_lat, mean_batch = extract_client_metrics(e)
         p99_lats.append(p99_lat)
-        p99_lat_errs.append(p99_lat_err)
         mean_batches.append(mean_batch)
 
     results_dict = {
@@ -159,7 +155,6 @@ def create_model_profile_df(results_dir):
         "mean_throughput_qps": mean_thrus,
         "std_throughput_qps": std_thrus,
         "p99_latency_ms": p99_lats,
-        "p99_latency_ms_stddev": p99_lat_errs,
         "mean_batch_size": mean_batches,
         "inst_type": inst_types,
         "cost": costs
@@ -170,3 +165,11 @@ def create_model_profile_df(results_dir):
     return df
 
 
+def load_profs(single_model_profs_dir=os.path.abspath("../results/single_model_profs/")):
+    profs = {}
+    for m in os.listdir(single_model_profs_dir):
+        print(m)
+        fname = os.path.join(single_model_profs_dir, m)
+        if os.path.isdir(fname):
+            profs[m] = create_model_profile_df(fname)
+    return profs
