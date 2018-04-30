2c0f5cf1efe3c3f16dea97c7c894106fb1edb4fd
Dan Crankshaw <dscrankshaw@gmail.com>
2018-04-30 22:37:19 +0000

    added new bucket for contention-tuned results

---
+++ results_cpp_benchmarker/e2e_results_contention_tuned/download_from_s3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+unset CDPATH
+# one-liner from http://stackoverflow.com/a/246128
+# Determines absolute path of the directory containing
+# the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# Let the user start this script from anywhere in the filesystem.
+cd $DIR/..
+
+aws s3 sync s3://inferline-results-contention-tuned $DIR
